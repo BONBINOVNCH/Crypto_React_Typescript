00438de03177f4af2d594486be60b129091614f5
MaksymZaluskiy <zmax1301@gmail.com>
2025-09-04 16:26:40 +0200

    toppers_some_info

--- src/components/Toppers.tsx
+++ src/components/Toppers.tsx
@@ -7,6 +7,7 @@ function Toppers() {
         name: string;
         image: string;
         current_price: number;
+        price_change_percentage_24h: number;
     }
 
     const data = useGetData<Coin[]>(
@@ -22,14 +23,26 @@ function Toppers() {
                 <div className="topper">
                     <img src={data[0].image} alt="" className="topper_img" />
                     <h5 className="topper_name">{data[0].name}</h5>
+                    <h5 className="topper_price">{data[0].current_price}$</h5>
+                    <h5 className="topper_change">
+                        {data[0].price_change_percentage_24h}%
+                    </h5>
                 </div>
                 <div className="topper">
                     <img src={data[1].image} alt="" className="topper_img" />
                     <h5 className="topper_name">{data[1].name}</h5>
+                    <h5 className="topper_price">{data[1].current_price}$</h5>
+                    <h5 className="topper_change">
+                        {data[1].price_change_percentage_24h}%
+                    </h5>
                 </div>
                 <div className="topper">
                     <img src={data[2].image} alt="" className="topper_img" />
                     <h5 className="topper_name">{data[2].name}</h5>
+                    <h5 className="topper_price">{data[2].current_price}$</h5>
+                    <h5 className="topper_change">
+                        {data[2].price_change_percentage_24h}%
+                    </h5>
                 </div>
             </div>
         );
