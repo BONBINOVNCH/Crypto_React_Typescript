3cd627d083193c95986d8fab1d9abb5540144d52
MaksymZaluskiy <zmax1301@gmail.com>
2025-09-08 13:34:44 +0200

    beta_pagination

--- src/data/useDataCoins.ts
+++ src/data/useDataCoins.ts
@@ -15,7 +15,7 @@ export default function useDataCoins() {
     }
 
     const data = useGetData<ExpandedCoin[]>(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&sparkline=false"
     );
     return data;
 }
