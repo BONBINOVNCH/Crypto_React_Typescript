455e88065933ab65ceb0bbfca4af93a968d5a9be
MaksymZaluskiy <zmax1301@gmail.com>
2025-09-11 15:42:31 +0200

    csv_file_download

--- src/components/crypto-dialog/data-table.tsx
+++ src/components/crypto-dialog/data-table.tsx
@@ -5,6 +5,8 @@ import * as React from "react";
 import { useStore } from "@/store";
 import { useEffect } from "react";
 
+import { CSVLink } from "react-csv";
+
 import {
     ColumnDef,
     ColumnFiltersState,
@@ -29,6 +31,7 @@ import {
 import { Input } from "@/components/ui/input";
 
 import { DataTablePagination } from "./DataTablePagination";
+import useDataCoins from "@/data/useDataCoins";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
@@ -66,81 +69,101 @@ export function DataTable<TData, TValue>({
         table.getColumn("name")?.setFilterValue(inputCoin);
     }, [inputCoin]);
 
-    return (
-        <div className="max-h-[100vh] overflow-y-hidden">
-            <div className="flex  justify-between items-center ">
-                <Input
-                    placeholder="Filter crypto..."
-                    value={inputCoin}
-                    onChange={(event) => {
-                        setInputCoin(event.target.value);
-                    }}
-                    className="max-w-sm"
-                />
+    const coinData = useDataCoins();
 
-                <img
-                    onClick={() => setOpen(false)}
-                    className="cross h-11"
-                    src=".\img\cross.png"
-                    alt=""
-                />
-            </div>
+    function csvLink() {
+        const headers = Object.keys(coinData![0] || {});
 
-            <div className="overflow-hidden rounded-md border max-h-[80vh] overflow-y-auto rounded-md bg-white p-4">
-                <Table>
-                    <TableHeader>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => {
-                                    return (
-                                        <TableHead key={header.id}>
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                      header.column.columnDef
-                                                          .header,
-                                                      header.getContext()
-                                                  )}
-                                        </TableHead>
-                                    );
-                                })}
-                            </TableRow>
-                        ))}
-                    </TableHeader>
-                    <TableBody>
-                        {table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map((row) => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={
-                                        row.getIsSelected() && "selected"
-                                    }
-                                >
-                                    {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext()
-                                            )}
-                                        </TableCell>
-                                    ))}
+        return [
+            headers.join(","),
+            ...coinData!.map((item) =>
+                headers
+                    .map((header) => item[header as keyof typeof item] ?? "")
+                    .join(",")
+            ),
+        ].join("\n");
+    }
+
+    if (coinData)
+        return (
+            <div className="max-h-[100vh] overflow-y-hidden">
+                <div className="flex  justify-between items-center ">
+                    <Input
+                        placeholder="Filter crypto..."
+                        value={inputCoin}
+                        onChange={(event) => {
+                            setInputCoin(event.target.value);
+                        }}
+                        className="max-w-sm"
+                    />
+
+                    <div>
+                        <CSVLink data={csvLink()}>fefefef</CSVLink>
+                    </div>
+
+                    <img
+                        onClick={() => setOpen(false)}
+                        className="cross h-11"
+                        src=".\img\cross.png"
+                        alt=""
+                    />
+                </div>
+
+                <div className="overflow-hidden rounded-md border max-h-[80vh] overflow-y-auto rounded-md bg-white p-4">
+                    <Table>
+                        <TableHeader>
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <TableRow key={headerGroup.id}>
+                                    {headerGroup.headers.map((header) => {
+                                        return (
+                                            <TableHead key={header.id}>
+                                                {header.isPlaceholder
+                                                    ? null
+                                                    : flexRender(
+                                                          header.column
+                                                              .columnDef.header,
+                                                          header.getContext()
+                                                      )}
+                                            </TableHead>
+                                        );
+                                    })}
                                 </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell
-                                    colSpan={columns.length}
-                                    className="h-24 text-center"
-                                >
-                                    No results.
-                                </TableCell>
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </div>
+                            ))}
+                        </TableHeader>
+                        <TableBody>
+                            {table.getRowModel().rows?.length ? (
+                                table.getRowModel().rows.map((row) => (
+                                    <TableRow
+                                        key={row.id}
+                                        data-state={
+                                            row.getIsSelected() && "selected"
+                                        }
+                                    >
+                                        {row.getVisibleCells().map((cell) => (
+                                            <TableCell key={cell.id}>
+                                                {flexRender(
+                                                    cell.column.columnDef.cell,
+                                                    cell.getContext()
+                                                )}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))
+                            ) : (
+                                <TableRow>
+                                    <TableCell
+                                        colSpan={columns.length}
+                                        className="h-24 text-center"
+                                    >
+                                        No results.
+                                    </TableCell>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </div>
 
-            <DataTablePagination table={table} />
-        </div>
-    );
+                <DataTablePagination table={table} />
+            </div>
+        );
 }
